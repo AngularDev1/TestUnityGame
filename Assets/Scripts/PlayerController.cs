5e873cb26a5b19b208bc72400b4caf3a623a0e0d
Roman Bolkun <roman.bolkun@honeycombsoft.com>
2020-06-04 10:03:02 +0300

    Added changes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text countText;
+
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Console.WriteLine("Triggered");
+        if (other.gameObject.CompareTag("Coin Item"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+}
